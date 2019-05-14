e4db3381d040b05f12fe4233811f902fc8cea3ec
aq1 <aq1@aq1.ru>
2019-05-14 19:28:29 +0300

    Change command

--- smm_admin/management/commands/make_a_post.py
+++ smm_admin/management/commands/make_a_post.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 
+from smm_admin.models import Post
 from smm_admin.tasks.make_a_post import (
     make_a_post,
 )
@@ -17,5 +18,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Working...\n'))
-        make_a_post(options['post_id'])
+        try:
+            post = Post.objects.get(id=options['post_id'])
+        except Post.DoesNotExist:
+            self.stderr.write(self.style.ERROR('Post not found'))
+            return
+        make_a_post(post)
         self.stdout.write(self.style.SUCCESS('Done.\n'))
